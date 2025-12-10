49d69b7ec162aefe056683944cd9c3f38cf7c244
Yu Jin <qldls0307@naver.com>
2025-12-10 15:26:42 +0900

    chore: about 수정

--- src/components/about-me.tsx
+++ src/components/about-me.tsx
@@ -1,13 +1,20 @@
 import { ABOUT_ME } from "../data";
-import { parseBold } from "../utils/parse-bold";
 import { Section } from "./Section";
+import { parseBold } from "../utils/parse-bold";
 
 export const AboutMe = () => {
   return (
     <Section title="About">
-      <div className="flex flex-col gap-3">
-        {ABOUT_ME.map((item, index) => (
-          <p key={index}>{parseBold(item)}</p>
+      <div className="flex flex-col gap-4">
+        {ABOUT_ME.map(({ title, descriptions }, index) => (
+          <div key={index}>
+            <h3 className="font-semibold text-lg mb-1">{title}</h3>
+            <div className="flex flex-col gap-2">
+              {descriptions.map((description, descIndex) => (
+                <p key={descIndex}>{parseBold(description)}</p>
+              ))}
+            </div>
+          </div>
         ))}
       </div>
     </Section>
