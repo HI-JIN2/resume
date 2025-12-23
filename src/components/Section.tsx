bdddb1415cb2989e0a711b5c6f952fa507ec6afd
Yu Jin <qldls0307@naver.com>
2025-12-23 17:13:54 +0900

    feat: 타이틀 포인트컬러 적용

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -12,7 +12,9 @@ export const Section = ({ title, children, mt, mb, style }: Props) => {
     <section style={{ marginTop: mt, marginBottom: mb, ...style }}>
       {title && (
         <header className="flex items-end mb-6">
-          <h3 className="inline-block text-4xl font-semibold">{title}</h3>
+          <h3 className="inline-block text-4xl font-semibold text-primary">
+            {title}
+          </h3>
           <div className="w-2 h-2 rounded-full bg-primary mb-1 ml-1.5" />
         </header>
       )}
