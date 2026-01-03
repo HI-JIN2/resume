42d65ef34e1ae90672dc2aa19357d098a7c3501b
Yu Jin <qldls0307@naver.com>
2026-01-03 18:17:31 +0900

    fix: 리다리엑트 오류 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useEffect } from "react";
+import { BrowserRouter, Routes, Route, Navigate, useSearchParams, useNavigate } from "react-router-dom";
 import { AboutMe } from "./components/about-me";
 import { LastUpdatedAt } from "./components/last-updated-at";
 import { Layout } from "./components/Layout";
@@ -10,21 +11,39 @@ import { Profile } from "./components/Profile";
 import { WorkExperience } from "./components/work-experience";
 import { ResumeProvider } from "./context/resume-context";
 
+function RedirectHandler() {
+  const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
+  
+  useEffect(() => {
+    // 404.html에서 전달한 redirect 쿼리 파라미터 처리
+    const redirect = searchParams.get('redirect');
+    if (redirect) {
+      navigate(redirect, { replace: true });
+    }
+  }, [searchParams, navigate]);
+  
+  return null;
+}
+
 function ResumeContent() {
   return (
-    <Layout>
-      <Profile />
-      <AboutMe />
-      <WorkExperience />
-      <PersonalProject />
-      <SimpleList title="Awards" sectionTitle="Award" sectionId="award" />
-      <OpenSourceProject title="Community" sectionTitle="Community" />
-      <Presentation />
-      <SimpleList title="Interview" sectionId="interview" />
-      <SimpleList title="Education" sectionId="education" />
-      <SimpleList title="Certificates" sectionId="certificates" />
-      <LastUpdatedAt />
-    </Layout>
+    <>
+      <RedirectHandler />
+      <Layout>
+        <Profile />
+        <AboutMe />
+        <WorkExperience />
+        <PersonalProject />
+        <SimpleList title="Awards" sectionTitle="Award" sectionId="award" />
+        <OpenSourceProject title="Community" sectionTitle="Community" />
+        <Presentation />
+        <SimpleList title="Interview" sectionId="interview" />
+        <SimpleList title="Education" sectionId="education" />
+        <SimpleList title="Certificates" sectionId="certificates" />
+        <LastUpdatedAt />
+      </Layout>
+    </>
   );
 }
 
